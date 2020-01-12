0e6ce976ff9e15827a8f05ed812ecf9a84006254
Petra Janouchova <petra.janouchova@gmail.com>
2020-01-12 16:08:26 +0100

    fixed dimensions everywhere and CRM vocab N/A added

---
+++ user_management/makeMultipleUsers.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+./makeUser.sh Adela Sobotkova adela@fedarch.org Pass.123
+./makeUser.sh Petra Hermankova petra@fedarch.org Pass.123
+./makeUser.sh Shawn Ross shawn@fedarch.org Pass.123
+./makeUser.sh Susan Lupack susan.lupack@mq.edu.au Pass.123
+./makeUser.sh Joanna Bennetts joanna.bennetts@students.mq.edu.au Pass.123
+./makeUser.sh Charles Boyd charles.boyd@students.mq.edu.au Pass.123
+./makeUser.sh Alison Carfi alison.carfi@students.mq.edu.au Pass.123
+./makeUser.sh Zedekia Griseh zedekiah.grisez@students.mq.edu.au Pass.123
+./makeUser.sh Alexandra Hewitt alexandra.hewitt@students.mq.edu.au Pass.123
+./makeUser.sh Michael Hitches michael.hitches@students.mq.edu.au Pass.123
+./makeUser.sh Reeham Khandkar reeham.khandkar@students.mq.edu.au Pass.123
+./makeUser.sh Dean Mcmah dean.mcmah@students.mq.edu.au Pass.123
+./makeUser.sh Samantha Mills samantha.mills2@students.mq.edu.au Pass.123
+./makeUser.sh David Morley david.morley@students.mq.edu.au Pass.123
+./makeUser.sh Brinnly Pegler brinnly.pegler@students.mq.edu.au Pass.123
+./makeUser.sh Sarah Roach sarah.roach@students.mq.edu.au Pass.123
+./makeUser.sh Nathan Stein nathan.stein@students.mq.edu.au Pass.123
+./makeUser.sh Lauren Wood lauren.wood1@students.mq.edu.au Pass.123
+./makeUser.sh Jacob Kiefel jacob.kiefel@students.mq.edu.au Pass.123
+
+
+
